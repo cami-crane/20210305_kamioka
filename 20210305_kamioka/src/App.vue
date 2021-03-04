92aa340734b9058530355f8c0991796162a6fdb9
cami-crane <76424633+cami-crane@users.noreply.github.com>
2021-03-05 02:51:01 +0900

    Add files via upload

---
+++ 20210305_kamioka/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+     <Adress></Adress>
+  </div>
+</template>
+
+<script>
+import Adress from "./components/Adress.vue";
+
+
+export default {
+  name: 'App',
+  components: {
+    Adress
+  }
+}
+</script>
---
+++ 20210305_kamioka/src/components/Adress.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="text" v-model="zip" />
+    <button @click="getAddress">住所自動入力</button>
+    <p>Adress: {{ address }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Adress",
+  data() {
+    return {
+      zip: ""
+    };
+  },
+  computed: {
+    ...mapState({
+      zip: state => state.zip
+    }),
+    address() {
+      return this.$store.state.zip.address;
+    }
+  },
+  methods: {
+    getAddress() {
+      this.$store.commit("getAddress", this.zip);
+      this.$store.dispatch("getAddressAction");
+    }
+  }
+};
+</script>
